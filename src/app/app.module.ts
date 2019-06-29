430c267ad7f5140543277344634956170032a730
Diego Chavez <diegochavezcarro@gmail.com>
2019-06-29 12:05:00 -0300

    2 way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent, PersonsComponent, PersonInputComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule, FormsModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
